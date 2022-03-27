b7b035fd0cb674cf5dcb59812531ba6f72274e9c
kyo <1339804425@qq.com>
2022-03-27 19:20:38 +0800

    Initial commit

---
+++ app01/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class user(models.Model):
+    name = models.CharField(max_length=32)
+    password = models.CharField(max_length=64)
+    age = models.IntegerField()
+class department(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=64)
+class userInfo(models.Model):
+    name = models.CharField(verbose_name="姓名",max_length=16)
+    password = models.CharField(verbose_name="密码",max_length=64)
+    age = models.IntegerField(verbose_name="年龄")
+    account = models.DecimalField("账户余额",max_digits=10,decimal_places=2,default=0)
+    creat_time = models.DateTimeField("入职时间")
+    depart = models.ForeignKey(to="department",to_field="id",on_delete=models.CASCADE)
+    gender_choices = (
+        (1,"男"),
+        (2,"女")
+    )
+    gender = models.SmallIntegerField(verbose_name="性别",choices=gender_choices)
---
+++ app01/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render,HttpResponse,redirect
+from app01.models import user
+from app01 import models
+
+# Create your views here.
+def login(request):
+    if request.method == 'GET':
+        return render(request,'login.html')
+    else:
+        #如果是POST请求，获取用户提交的数据
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        obj = user.objects.filter(name=username).first()#获取账号对应的数据表
+        print(obj)
+        if obj != None:
+            if username == obj.name and password == obj.password:
+                return HttpResponse("登陆成功")
+            else:
+                return render(request, 'login.html', {'error_msg': '账号名或密码错误'})
+        else:
+            return render(request, 'login.html', {'error_msg': '账号不存在'})
+def orm(request):
+    user.objects.create(name='wlj',password='520999',age=20)#增
+    #删查
+    # user.objects.filter(id=1).delete()
+    # user.objects.all()
+    return HttpResponse("成功")
+def user_list(request):
+    querset = models.department.objects.all()
+    return render(request,'user_list.html',{'querset':querset})#根据App的注册顺序主意去他们的templates去找
+def department_add(request):
+    if request.method == 'GET':
+        return render(request,'department_add.html')
+    else:
+        id = request.POST.get('id')
+        name = request.POST.get('name')
+        if models.department.objects.filter(id=id).first() == None and models.department.objects.filter(name=name).first() == None:
+            models.department.objects.create(id=id,name=name)
+        return redirect('/user/list')
+def department_delete(request):
+    nid = request.GET.get('nid')
+    models.department.objects.filter(id=nid).delete()
+    return redirect('/user/list')
+def department_edit(request,nid):
+    if request.method == 'GET':
+        obj = models.department.objects.filter(id=nid).first()
+        return render(request, 'department_edit.html',{'obj':obj})
+    id = request.POST.get('id')
+    name = request.POST.get('name')
+    if id == '' or models.department.objects.filter(id=id).first() != None:
+        id = nid
+    if name != '':
+        models.department.objects.filter(id=nid).update(id=id,name=name)
+    else:
+        models.department.objects.filter(id=nid).update(id=id)
+    return redirect('/user/list')
